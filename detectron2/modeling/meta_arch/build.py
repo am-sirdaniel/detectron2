8198d8cb76e5d08bfb5c1fb2c13f71e3c6649033
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-07-26 00:34:05 +0200

    Update build.py

--- detectron2/modeling/meta_arch/build.py
+++ detectron2/modeling/meta_arch/build.py
@@ -12,99 +12,99 @@ The registered object will be called with `obj(cfg)`
 and expected to return a `nn.Module` object.
 """
 
-# def weight_init(m):
-#     if isinstance(m, nn.Linear):
-#         nn.init.kaiming_normal(m.weight)
+def weight_init(m):
+    if isinstance(m, nn.Linear):
+        nn.init.kaiming_normal(m.weight)
 
         
 
-# class Linear(nn.Module):
-#     def __init__(self, linear_size, p_dropout=0.5):
-#         super(Linear, self).__init__()
-#         self.l_size = linear_size
+class Linear(nn.Module):
+    def __init__(self, linear_size, p_dropout=0.5):
+        super(Linear, self).__init__()
+        self.l_size = linear_size
 
-#         self.relu = nn.ReLU(inplace=True)
-#         self.dropout = nn.Dropout(p_dropout)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(p_dropout)
 
-#         self.w1 = nn.Linear(self.l_size, self.l_size)
-#         self.batch_norm1 = nn.BatchNorm1d(self.l_size)
+        self.w1 = nn.Linear(self.l_size, self.l_size)
+        self.batch_norm1 = nn.BatchNorm1d(self.l_size)
 
-#         self.w2 = nn.Linear(self.l_size, self.l_size)
-#         self.batch_norm2 = nn.BatchNorm1d(self.l_size)
+        self.w2 = nn.Linear(self.l_size, self.l_size)
+        self.batch_norm2 = nn.BatchNorm1d(self.l_size)
 
-#     def forward(self, x):
-#         y = self.w1(x)
-#         y = self.batch_norm1(y)
-#         y = self.relu(y)
-#         y = self.dropout(y)
+    def forward(self, x):
+        y = self.w1(x)
+        y = self.batch_norm1(y)
+        y = self.relu(y)
+        y = self.dropout(y)
 
-#         y = self.w2(y)
-#         y = self.batch_norm2(y)
-#         y = self.relu(y)
-#         y = self.dropout(y)
+        y = self.w2(y)
+        y = self.batch_norm2(y)
+        y = self.relu(y)
+        y = self.dropout(y)
 
-#         out = x + y
+        out = x + y
 
-#         return out
+        return out
 
 
-# class LinearModel(nn.Module):
-#     def __init__(self,
-#                  linear_size=1024,
-#                  num_stage=2,
-#                  p_dropout=0.5):
-#         super(LinearModel, self).__init__()
+class LinearModel(nn.Module):
+    def __init__(self,
+                 linear_size=1024,
+                 num_stage=2,
+                 p_dropout=0.5):
+        super(LinearModel, self).__init__()
 
-#         self.linear_size = linear_size
-#         self.p_dropout = p_dropout
-#         self.num_stage = num_stage
+        self.linear_size = linear_size
+        self.p_dropout = p_dropout
+        self.num_stage = num_stage
 
-#         # 2d joints
-#         self.input_size =  6 * 2
-#         # 3d joints
-#         self.output_size = 6 * 3
+        # 2d joints
+        self.input_size =  6 * 2
+        # 3d joints
+        self.output_size = 6 * 3
 
-#         # process input to linear size
-#         self.w1 = nn.Linear(self.input_size, self.linear_size)
-#         self.batch_norm1 = nn.BatchNorm1d(self.linear_size)
+        # process input to linear size
+        self.w1 = nn.Linear(self.input_size, self.linear_size)
+        self.batch_norm1 = nn.BatchNorm1d(self.linear_size)
 
-#         self.linear_stages = []
-#         for l in range(num_stage):
-#             self.linear_stages.append(Linear(self.linear_size, self.p_dropout))
-#         self.linear_stages = nn.ModuleList(self.linear_stages)
+        self.linear_stages = []
+        for l in range(num_stage):
+            self.linear_stages.append(Linear(self.linear_size, self.p_dropout))
+        self.linear_stages = nn.ModuleList(self.linear_stages)
 
-#         # post processing
-#         self.w2 = nn.Linear(self.linear_size, self.output_size)
+        # post processing
+        self.w2 = nn.Linear(self.linear_size, self.output_size)
 
-#         self.relu = nn.ReLU(inplace=True)
-#         self.dropout = nn.Dropout(self.p_dropout)
+        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(self.p_dropout)
 
-#     def forward(self, x):
-#         # pre-processing
-#         y = self.w1(x)
-#         y = self.batch_norm1(y)
-#         y = self.relu(y)
-#         y = self.dropout(y)
+    def forward(self, x):
+        # pre-processing
+        y = self.w1(x)
+        y = self.batch_norm1(y)
+        y = self.relu(y)
+        y = self.dropout(y)
 
-#         # linear layers
-#         for i in range(self.num_stage):
-#             y = self.linear_stages[i](y)
+        # linear layers
+        for i in range(self.num_stage):
+            y = self.linear_stages[i](y)
 
-#         y = self.w2(y)
+        y = self.w2(y)
 
-#         return y
+        return y
 
-# class CombinedWithLinear(nn.Module):
-# 	"""docstring for Combined"""
-# 	def __init__(self, maskrcc_model):
-# 		super(CombinedWithLinear, self).__init__()
-# 		self.maskrcc_model = maskrcc_model
-# 		self.linearmodel = LinearModel()
+class CombinedWithLinear(nn.Module):
+	"""docstring for Combined"""
+	def __init__(self, maskrcc_model):
+		super(CombinedWithLinear, self).__init__()
+		self.maskrcc_model = maskrcc_model
+		self.linearmodel = LinearModel()
 
-# 	def foward(x):
-# 		x = maskrcc_model(x)
-# 		x = linearmodel(x)
-# 		return x
+	def foward(x):
+		x = maskrcc_model(x)
+		x = linearmodel(x)
+		return x
 
 
 def build_model(cfg):
