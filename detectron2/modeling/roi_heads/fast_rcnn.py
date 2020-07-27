8e43527f65b174f651f555dbf85792c320cffa52
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-07-28 00:28:55 +0200

    Update fast_rcnn.py

--- detectron2/modeling/roi_heads/fast_rcnn.py
+++ detectron2/modeling/roi_heads/fast_rcnn.py
@@ -226,6 +226,7 @@ class FastRCNNOutputs(object):
                 reduction="sum",
             )
         else:
+            print('self.pred_class_logits',self.pred_class_logits)
             self._log_accuracy()
             return F.cross_entropy(self.pred_class_logits, self.gt_classes, reduction="mean")
 
