a184c4b4dec02f88d119c77da18b541132414712
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-07-28 03:47:30 +0200

    Update keypoint_head.py

--- detectron2/modeling/roi_heads/keypoint_head.py
+++ detectron2/modeling/roi_heads/keypoint_head.py
@@ -113,7 +113,7 @@ def integral_2d_innovate(heatmap, rois):
     j_ = torch.sum(j*h_norm, dim=(-1,-2))
 
     # transforming back to global relative coords
-    print('i_, scale_inv_x, start_x', i_.shape, scale_inv_x.shape, start_x.shape)
+    print('i_, scale_inv_x, start_x', i_.shape, scale_inv_x, start_x)
     print('i_ (before) as 0-1 coordinates', i_[0])
     # i_ = i_ * scale_inv_x.reshape(-1,1) + start_x.reshape(-1,1)
     # j_ = j_ * scale_inv_y.reshape(-1,1) + start_y.reshape(-1,1)
