a0d3cfd45aa84c20ae3192232db44c038ff534e0
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-08-03 22:04:16 +0200

    Update keypoint_head.py

--- detectron2/modeling/roi_heads/keypoint_head.py
+++ detectron2/modeling/roi_heads/keypoint_head.py
@@ -520,11 +520,11 @@ def keypoint_rcnn_inference(pred_keypoint_logits, pred_instances):
     num_instances_per_image = [len(i) for i in pred_instances]
     keypoint_results = keypoint_results[:, :, [0, 1, 3]].split(num_instances_per_image, dim=0)
     try:
-	    print('pred keypoint_results after split', keypoint_results.tensor.shape)
+    	print('pred keypoint_results after split', keypoint_results.tensor.shape)
 	    print('sample pred keypoint_results after split', keypoint_results.tensor[0][0])
 	    print('pred_instances', len(pred_instances))
-	except:
-		pass
+    except:
+    	pass
 
     for keypoint_results_per_image, instances_per_image in zip(keypoint_results, pred_instances):
         # keypoint_results_per_image is (num instances)x(num keypoints)x(x, y, score)
