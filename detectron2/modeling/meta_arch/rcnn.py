60f878f3ea98a79881e6bf7e2db694d65fb7b96f
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-08-11 15:56:42 +0200

    Update rcnn.py

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -189,6 +189,7 @@ class GeneralizedRCNN(nn.Module):
         """
         Normalize, pad and batch the input images.
         """
+        print([x["image"] for x in batched_inputs])
         images = [x["image"].to(self.device) for x in batched_inputs]
         images = [(x - self.pixel_mean) / self.pixel_std for x in images]
         images = ImageList.from_tensors(images, self.backbone.size_divisibility)
