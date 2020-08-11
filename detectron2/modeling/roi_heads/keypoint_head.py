d5bec0aa61a52c419354c3680ae2c18dc1ba081d
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-08-11 16:54:11 +0200

    Update keypoint_head.py

--- detectron2/modeling/roi_heads/keypoint_head.py
+++ detectron2/modeling/roi_heads/keypoint_head.py
@@ -563,11 +563,12 @@ class BaseKeypointRCNNHead(nn.Module):
         self.loss_normalizer = loss_normalizer
         self.linearmodel = LinearModel()
         try:
-            print('self.device', self.device)
-            self.linearmodel.to(self.device)
+            print('using torch.device("cuda")')
+            self.linearmodel.to(torch.device("cuda"))
         except:
             print('using cuda directly')
             self.linearmodel = self.linearmodel.cuda()
+        
         self.linearmodel.apply(weight_init)
         print(">>> total params: {:.2f}M".format(sum(p.numel() for p in self.linearmodel.parameters()) / 1000000.0))
         
