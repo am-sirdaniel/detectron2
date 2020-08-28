83ce284d1814a959bdf4ebe034b1e3851b1159f3
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-08-28 13:35:23 +0200

    Update coco_evaluation.py

--- detectron2/evaluation/coco_evaluation.py
+++ detectron2/evaluation/coco_evaluation.py
@@ -539,7 +539,7 @@ def _evaluate_predictions_on_coco(
         # we just take the first entry from both
 
         print('coco_results[0]', coco_results[0]['pred_3d_pts'])
-        print('next(iter(coco_gt)', coco_gt.keys())
+        print('next(iter(coco_gt)', coco_gt)
         print('next(iter(coco_gt.anns.values()))', next(iter(coco_gt.anns.values())))
 
 
