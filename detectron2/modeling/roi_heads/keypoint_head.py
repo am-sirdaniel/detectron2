8098de91acf6ffce61d350f5f0aa65e844c756ab
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-09-13 00:18:25 +0200

    Update keypoint_head.py

--- detectron2/modeling/roi_heads/keypoint_head.py
+++ detectron2/modeling/roi_heads/keypoint_head.py
@@ -620,7 +620,7 @@ def keypoint_rcnn_inference(pred_keypoint_logits, pred_instances):
         #print('instances_per_image:', instances_per_image)
         instances_per_image.pred_keypoints = keypoint_results_per_image
         instances_per_image.pred_3d_pts = pred_3d_results_per_image #.unsqueeze(0)
-        print('pred_3d_results_per_image sample', pred_3d_results_per_image[0])
+        #print('pred_3d_results_per_image sample', pred_3d_results_per_image[0])
 
     
 
