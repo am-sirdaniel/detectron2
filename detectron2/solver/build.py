a08cca2f7a70000fe85a38c86a2441ac27e2f23e
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-09-23 12:56:45 +0200

    Update build.py

--- detectron2/solver/build.py
+++ detectron2/solver/build.py
@@ -115,18 +115,11 @@ def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimiz
         #print(idx, '-->', module)
         print('module: ', module)
         for key, value in module.named_parameters(recurse=False):
-            # if 167 <= idx <= 184:
-            #     #Set 2nd architecture layers to False
-            #     value.requires_grad = False
-                
-            # if 167 > idx > 184:
-            #     #Set 1st architecture layers to False
-            #     value.requires_grad = False
 
             if 'linearmodel' in idx.split('.'):
-                value.requires_grad = True
-            else:
                 value.requires_grad = False
+            else:
+                value.requires_grad = True
 
             print('idx', idx, ', key: ',key, ', value.requires_grad:', value.requires_grad)
             #print('value',value[0])
