ef3b6d440430bd24af81636a29b5cb9ef434ae09
Daniel Ajisafe <ajisafedaniel@gmail.com>
2020-10-05 18:20:37 +0200

    Update build.py

--- detectron2/solver/build.py
+++ detectron2/solver/build.py
@@ -116,10 +116,10 @@ def build_optimizer(cfg: CfgNode, model: torch.nn.Module) -> torch.optim.Optimiz
         print('module: ', module)
         for key, value in module.named_parameters(recurse=False):
 
-            # if 'linearmodel' in idx.split('.'): # 3D
-            #     value.requires_grad = False
-            # else:
-            #     value.requires_grad = True  # 2D
+            if 'linearmodel' in idx.split('.'): # 3D
+                value.requires_grad = False
+            else:
+                value.requires_grad = True  # 2D
 
             print('idx', idx, ', key: ',key, ', value.requires_grad:', value.requires_grad)
             #print('value',value[0])
